8d18e0cc2438bcebb19f531bf74ab539cb816c6f
Corey Tokunaga-Reichert <315402@bloodsystems.org>
2025-08-28 19:30:30 -0600

    AB#21388 Changed description property in images model to action property to store link that image will use when clicked

--- Models/ImagesModel.cs
+++ Models/ImagesModel.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        public string? Description { get; set; }
         public string? ImageURL { get; set; }
+        public string? Action { get; set; }
         public DateTime PublishFromDate { get; set; }
         public DateTime PublishToDate { get; set; }
         public bool IsActive { get; set; }
