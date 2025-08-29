5d93d20d8ae8a5077b43e44c977c042d8c8d1dba
Corey Tokunaga-Reichert <315402@bloodsystems.org>
2025-08-28 19:28:31 -0600

    AB#21387 Updated home controller to use configuration interface to allow for access to settings and connection strings in app settings

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Mobile_CMS.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -25,7 +26,6 @@ namespace Mobile_CMS.Controllers
             return View();
         }
 
-        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
